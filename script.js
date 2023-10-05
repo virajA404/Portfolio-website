0c5ccb2799d493beb6beb05133cda209b9a54764
Viraj Abeywardana <abeywardana21@gmail.com>
2023-10-05 14:31:43 +0530

    contact

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function sendEmail(){
         To : 'abeywardana21@gmail.com',
         From : document.getElementById("email").value,
         Subject : "New contact form enquiry",
-        Body : "And this is the body"
+        Body : "Name: " + document.getElementById("name").value
+               + " <br> Email: " + document.getElementById("email").value
+               + " <br> Message: " + document.getElementById("message").value
     }).then(
       message => alert("Message sent successfully!")
     );
